806373dd7e66d52675ecc4c43c1531959719adee
Antonio Costa <antcosta@redhat.com>
2022-06-08 10:37:31 +0200

    adapt to dekorate BOM

--- run_tests_with_dekorate.sh
+++ run_tests_with_dekorate.sh
@@ -10,5 +10,18 @@ if [[ $(waitFor "sso" "application") -eq 1 ]] ; then
 fi
 SSO_URL=$(oc get route secure-sso -o jsonpath='https://{.spec.host}/auth')
 
+# 3.- Capture SpringBoot version
+SB_VERSION_SWITCH=""
+
+while getopts v: option
+do
+    case "${option}"
+        in
+        v)SB_VERSION_SWITCH="-Dspring-boot.version=${OPTARG}";;
+    esac
+done
+
+echo "SB_VERSION_SWITCH: ${SB_VERSION_SWITCH}"
+
 # Run OpenShift Tests
-./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it -DSSO_AUTH_SERVER_URL=$SSO_URL
+eval "./mvnw -s .github/mvn-settings.xml clean verify -Popenshift,openshift-it -DSSO_AUTH_SERVER_URL=$SSO_URL ${SB_VERSION_SWITCH}"
